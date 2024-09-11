fb0a6df93131d9bb71f47c6c1019f9052cd43651
PatrikFlooCZ <89390804+PatrikFlooCZ@users.noreply.github.com>
2024-09-11 09:02:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,15 @@
         console.log('Dveře zavřeny');
         doorStatus.textContent = 'Dveře jsou zavřeny';
     });
+
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then((registration) => {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, (err) => {
+        console.log('ServiceWorker registration failed: ', err);
+      });
+  });
+}
