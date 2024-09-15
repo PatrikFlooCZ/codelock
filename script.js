4e5654f68e37b44ffe616b0db5e0714acfbeabbe
PatrikFlooCZ <89390804+PatrikFlooCZ@users.noreply.github.com>
2024-09-15 19:10:40 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
     const doorStatus = document.getElementById('doorStatus');
+    const body = document.body;
 
     document.getElementById('openButton').addEventListener('click', function () {
         console.log('Dveře otevřeny');
         doorStatus.textContent = 'Dveře jsou otevřeny';
+        body.classList.remove('closed');
+        body.classList.add('open');
     });
 
     document.getElementById('closeButton').addEventListener('click', function () {
         console.log('Dveře zavřeny');
         doorStatus.textContent = 'Dveře jsou zavřeny';
+        body.classList.remove('open');
+        body.classList.add('closed');
     });
-
+});
